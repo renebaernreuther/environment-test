c44d4748a5b2de7bbdca52eac6611141053e0d64
renebaernreuther <61974989+renebaernreuther@users.noreply.github.com>
2023-03-03 11:27:31 +0100

    Update EnvTestApplication.java

--- src/main/java/de/vemaeg/envtest/EnvTestApplication.java
+++ src/main/java/de/vemaeg/envtest/EnvTestApplication.java
@@ -2,7 +2,7 @@ package de.vemaeg.envtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 
 @SpringBootApplication
 public class EnvTestApplication {
 
